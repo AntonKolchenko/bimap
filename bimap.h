e66fdd042b26018c1a3f1c6a22393b877520dc0a
admin <admin@admins-MacBook-Pro.local>
2022-11-25 14:47:45 +0300

    the 1st release

--- bimap.h
+++ bimap.h
@@ -1,129 +1,450 @@
 #pragma once
 
-#include <cstddef>
+#include <type_traits>
 
-template <typename Left, typename Right, typename CompareLeft,
-          typename CompareRight>
+#include "set.h"
+
+template <typename Left, typename Right, typename CompareLeft = std::less<Left>,
+          typename CompareRight = std::less<Right>>
 struct bimap {
-  using left_t = ...;
-  using right_t = ...;
 
-  using node_t = ...;
+private:
+  struct LEFT_TAG;
+  struct RIGHT_TAG;
 
-  struct right_iterator; // По аналогии с left_iterator
-  struct left_iterator {
-    // Элемент на который сейчас ссылается итератор.
-    // Разыменование итератора end_left() неопределено.
-    // Разыменование невалидного итератора неопределено.
-    left_t const &operator*() const;
-    left_t const *operator->() const;
-
-    // Переход к следующему по величине left'у.
-    // Инкремент итератора end_left() неопределен.
-    // Инкремент невалидного итератора неопределен.
-    left_iterator &operator++();
-    left_iterator operator++(int);
-
-    // Переход к предыдущему по величине left'у.
-    // Декремент итератора begin_left() неопределен.
-    // Декремент невалидного итератора неопределен.
-    left_iterator &operator--();
-    left_iterator operator--(int);
-
-    // left_iterator ссылается на левый элемент некоторой пары.
-    // Эта функция возвращает итератор на правый элемент той же пары.
-    // end_left().flip() возращает end_right().
-    // end_right().flip() возвращает end_left().
-    // flip() невалидного итератора неопределен.
-    right_iterator flip() const;
+  template <class T, class Tag>
+  struct node_element : intrusive::set_element_base {
+    T value;
+    node_element(T value)
+        : value(std::move(value)), intrusive::set_element_base() {}
   };
+
+  using left_t = Left;
+  using right_t = Right;
+
+  struct node : node_element<Left, LEFT_TAG>, node_element<Right, RIGHT_TAG> {
+    node(Left left, Right right)
+        : node_element<Left, LEFT_TAG>(std::move(left)),
+          node_element<Right, RIGHT_TAG>(std::move(right)) {}
+  };
+
+  using node_t = node;
+
+  std::size_t bimap_size = 0;
+  intrusive::set<node_element<Left, LEFT_TAG>, Left, LEFT_TAG, CompareLeft>
+      left_set;
+  intrusive::set<node_element<Right, RIGHT_TAG>, Right, RIGHT_TAG, CompareRight>
+      right_set;
+
+public:
+  template <class iterator_t, class iterator_tag, class other_iterator_t,
+            class other_iterator_tag>
+  struct base_iterator {
+    using iterator_category = std::bidirectional_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = iterator_t;
+    using pointer = iterator_t*;
+    using reference = iterator_t&;
+
+    using tmp_node_lca_t =
+        std::conditional_t<std::is_const_v<iterator_t>, node const, node>;
+
+    using tmp_node_t =
+        std::conditional_t<std::is_const_v<iterator_t>,
+                           node_element<iterator_t, iterator_tag> const,
+                           node_element<iterator_t, iterator_tag>>;
+    using other_tmp_node_t = std::conditional_t<
+        std::is_const_v<iterator_t>,
+        node_element<other_iterator_t, other_iterator_tag> const,
+        node_element<other_iterator_t, other_iterator_tag>>;
+
+    base_iterator() = default;
+
+    base_iterator(intrusive::set_element_base* ptr) : ptr(ptr) {}
+
+    base_iterator& operator++() {
+      ptr = intrusive::next(ptr);
+      return *this;
+    }
+    base_iterator operator++(int) {
+      auto tmp = *this;
+      ptr = intrusive::next(ptr);
+      return tmp;
+    }
+
+    base_iterator& operator--() {
+      ptr = intrusive::prev(ptr);
+      return *this;
+    }
+    base_iterator operator--(int) {
+      auto tmp = *this;
+      ptr = intrusive::prev(ptr);
+      return tmp;
+    }
+
+    reference operator*() const {
+      return static_cast<node_element<iterator_t, iterator_tag>&>(*ptr).value;
+    }
+    pointer operator->() const {
+      return &operator*();
+    }
+
+    using other_type_iterator =
+        base_iterator<other_iterator_t, other_iterator_tag, iterator_t,
+                      iterator_tag>;
+    other_type_iterator flip() const {
+      auto* tmp_node = static_cast<tmp_node_t*>(ptr);
+      auto* tmp_lca_node = static_cast<tmp_node_lca_t*>(tmp_node);
+      auto* other_tmp_node = static_cast<other_tmp_node_t*>(tmp_lca_node);
+      return other_type_iterator(
+          static_cast<intrusive::set_element_base*>(other_tmp_node));
+    }
+
+    intrusive::set_element_base* get_ptr() const {
+      return ptr;
+    }
+
+    friend bool operator==(base_iterator const& left,
+                           base_iterator const& right) {
+      return left.ptr == right.ptr;
+    }
+    friend bool operator!=(base_iterator& left, base_iterator& right) {
+      return !(left == right);
+    }
+
+  private:
+    intrusive::set_element_base* ptr = nullptr;
+  };
+
+  using left_iterator = base_iterator<left_t, LEFT_TAG, right_t, RIGHT_TAG>;
+  using right_iterator = base_iterator<right_t, RIGHT_TAG, left_t, LEFT_TAG>;
 
   // Создает bimap не содержащий ни одной пары.
   bimap(CompareLeft compare_left = CompareLeft(),
-        CompareRight compare_right = CompareRight());
+        CompareRight compare_right = CompareRight())
+      : left_set(std::move(compare_left)), right_set(std::move(compare_right)) {
+  }
 
   // Конструкторы от других и присваивания
-  bimap(bimap const &other);
-  bimap(bimap &&other) noexcept;
+  bimap(bimap const& other)
+      : left_set(CompareLeft()), right_set(CompareRight()), bimap_size(0) {
+    for (auto it = other.begin_left(); it != other.end_left(); it++) {
+      insert(*it, *it.flip());
+    }
+  }
 
-  bimap &operator=(bimap const &other);
-  bimap &operator=(bimap &&other) noexcept;
+  bimap(bimap&& other) noexcept = default;
+
+  bimap& operator=(bimap const& other) {
+    if (*this != other) {
+      bimap<Left, Right, CompareLeft, CompareRight>(other).swap(*this);
+    }
+    return *this;
+  }
+
+  bimap& operator=(bimap&& other) noexcept {
+    if (*this != other) {
+      bimap<Left, Right, CompareLeft, CompareRight>(std::move(other))
+          .swap(*this);
+    }
+    return *this;
+  }
+
+  void swap(bimap& other) {
+    bimap tmp(*this);
+
+    erase_left(begin_left(), end_left());
+    for (auto it = other.begin_left(); it != other.end_left(); it++) {
+      insert(*it, *it.flip());
+    }
+
+    other.erase_left(other.begin_left(), other.end_left());
+    for (auto it = tmp.begin_left(); it != tmp.end_left(); it++) {
+      other.insert(*it, *it.flip());
+    }
+  }
 
   // Деструктор. Вызывается при удалении объектов bimap.
   // Инвалидирует все итераторы ссылающиеся на элементы этого bimap
   // (включая итераторы ссылающиеся на элементы следующие за последними).
-  ~bimap();
+  ~bimap() {
+    erase_left(begin_left(), end_left());
+  }
 
   // Вставка пары (left, right), возвращает итератор на left.
   // Если такой left или такой right уже присутствуют в bimap, вставка не
   // производится и возвращается end_left().
-  left_iterator insert(left_t const &left, right_t const &right);
-  left_iterator insert(left_t const &left, right_t &&right);
-  left_iterator insert(left_t &&left, right_t const &right);
-  left_iterator insert(left_t &&left, right_t &&right);
+  left_iterator insert(left_t const& left, right_t const& right) {
+    if (find_left(left) != end_left() || find_right(right) != end_right()) {
+      return end_left();
+    }
+
+    node_t* pointer = new node_t(left, right);
+
+    auto& l_node = static_cast<node_element<Left, LEFT_TAG>&>(*pointer);
+    auto& r_node = static_cast<node_element<Right, RIGHT_TAG>&>(*pointer);
+
+    right_set.insert(r_node);
+    left_set.insert(l_node);
+
+    bimap_size++;
+    return left_iterator(left_set.find_ptr(l_node.value));
+  }
+  left_iterator insert(left_t const& left, right_t&& right) {
+    if (find_left(left) != end_left() || find_right(right) != end_right()) {
+      return end_left();
+    }
+
+    node_t* pointer = new node_t(left, std::move(right));
+
+    auto& l_node = static_cast<node_element<Left, LEFT_TAG>&>(*pointer);
+    auto& r_node = static_cast<node_element<Right, RIGHT_TAG>&>(*pointer);
+
+    bimap_size++;
+    right_set.insert(r_node);
+    left_set.insert(l_node);
+
+    return left_iterator(left_set.find_ptr(l_node.value));
+  }
+  left_iterator insert(left_t&& left, right_t const& right) {
+    if (find_left(left) != end_left() || find_right(right) != end_right()) {
+      return end_left();
+    }
+
+    node_t* pointer = new node_t(std::move(left), right);
+
+    auto& l_node = static_cast<node_element<Left, LEFT_TAG>&>(*pointer);
+    auto& r_node = static_cast<node_element<Right, RIGHT_TAG>&>(*pointer);
+
+    bimap_size++;
+    right_set.insert(r_node);
+    left_set.insert(l_node);
+
+    return left_iterator(left_set.find_ptr(l_node.value));
+  }
+  left_iterator insert(left_t&& left, right_t&& right) {
+    if (find_left(left) != end_left() || find_right(right) != end_right()) {
+      return end_left();
+    }
+
+    node_t* pointer = new node_t(std::move(left), std::move(right));
+
+    auto& l_node = static_cast<node_element<Left, LEFT_TAG>&>(*pointer);
+    auto& r_node = static_cast<node_element<Right, RIGHT_TAG>&>(*pointer);
+
+    bimap_size++;
+    right_set.insert(r_node);
+    left_set.insert(l_node);
+
+    return left_iterator(left_set.find_ptr(l_node.value));
+  }
+
+  void remove(left_iterator it) {
+    bimap_size--;
+
+    auto* ptr_tmp_node =
+        static_cast<node_element<Left, LEFT_TAG>*>(it.get_ptr());
+    auto* ptr_node = static_cast<node*>(ptr_tmp_node);
+
+    auto& left_value = *it;
+    auto& right_value = *(it.flip());
+
+    left_set.erase(left_value);
+    right_set.erase(right_value);
+
+    delete ptr_node;
+  }
+  void remove(right_iterator it) {
+    bimap_size--;
+
+    auto* ptr_tmp_node =
+        static_cast<node_element<Right, RIGHT_TAG>*>(it.get_ptr());
+    auto* ptr_node = static_cast<node*>(ptr_tmp_node);
+
+    auto& right_value = *it;
+    auto& left_value = *it.flip();
+
+    left_set.erase(left_value);
+    right_set.erase(right_value);
+
+    delete ptr_node;
+  }
 
   // Удаляет элемент и соответствующий ему парный.
   // erase невалидного итератора неопределен.
   // erase(end_left()) и erase(end_right()) неопределены.
   // Пусть it ссылается на некоторый элемент e.
   // erase инвалидирует все итераторы ссылающиеся на e и на элемент парный к e.
-  left_iterator erase_left(left_iterator it);
+  left_iterator erase_left(left_iterator it) {
+    auto tmp = it++;
+    remove(tmp);
+    return it;
+  }
+
   // Аналогично erase, но по ключу, удаляет элемент если он присутствует, иначе
   // не делает ничего Возвращает была ли пара удалена
-  bool erase_left(left_t const &left);
+  bool erase_left(left_t const& left) {
+    auto it = find_left(left);
+    if (it == end_left()) {
+      return false;
+    }
+    remove(it);
+    return true;
+  }
 
-  right_iterator erase_right(right_iterator it);
-  bool erase_right(right_t const &right);
+  right_iterator erase_right(right_iterator it) {
+    auto tmp = it++;
+    remove(tmp);
+    return it;
+  }
+  bool erase_right(right_t const& right) {
+    auto it = find_right(right);
+    if (it == end_right()) {
+      return false;
+    }
+    remove(it);
+    return true;
+  }
 
   // erase от ренжа, удаляет [first, last), возвращает итератор на последний
   // элемент за удаленной последовательностью
-  left_iterator erase_left(left_iterator first, left_iterator last);
-  right_iterator erase_right(right_iterator first, right_iterator last);
+  left_iterator erase_left(left_iterator first, left_iterator last) {
+    while (first != last) {
+      auto tmp_it = first++;
+      remove(tmp_it);
+    }
+    return first;
+  }
+  right_iterator erase_right(right_iterator first, right_iterator last) {
+    while (first != last) {
+      auto tmp_it = first++;
+      remove(tmp_it);
+    }
+    return first;
+  }
 
   // Возвращает итератор по элементу. Если не найден - соответствующий end()
-  left_iterator find_left(left_t const &left) const;
-  right_iterator find_right(right_t const &right) const;
+  left_iterator find_left(left_t const& left) const {
+    return left_iterator(left_set.find_ptr(left));
+  }
+  right_iterator find_right(right_t const& right) const {
+    return right_iterator(right_set.find_ptr(right));
+  }
 
   // Возвращает противоположный элемент по элементу
   // Если элемента не существует -- бросает std::out_of_range
-  right_t const &at_left(left_t const &key) const;
-  left_t const &at_right(right_t const &key) const;
+  Right const& at_left(left_t const& key) const {
+    auto it = find_left(key);
+    if (it == end_left()) {
+      throw std::out_of_range(
+          "left element wasn't found at 'at_left' function");
+    }
+    return *it.flip();
+  }
+  Left const& at_right(right_t const& key) const {
+    auto it = find_right(key);
+    if (it == end_right()) {
+      throw std::out_of_range(
+          "right element wasn't found at 'at_right' function");
+    }
+    return *it.flip();
+  }
 
   // Возвращает противоположный элемент по элементу
   // Если элемента не существует, добавляет его в bimap и на противоположную
   // сторону кладет дефолтный элемент, ссылку на который и возвращает
   // Если дефолтный элемент уже лежит в противоположной паре - должен поменять
   // соответствующий ему элемент на запрашиваемый (смотри тесты)
-  right_t const &at_left_or_default(left_t const &key);
-  left_t const &at_right_or_default(right_t const &key);
+
+  template <typename T = Right,
+            std::enable_if_t<std::is_default_constructible_v<T>, int> = 0>
+  right_t const& at_left_or_default(left_t const& key) {
+    auto it = find_left(key);
+    if (it == end_left()) {
+      if (find_right(right_t()) != end_right()) {
+        erase_right(right_t());
+      }
+      it = insert(key, right_t());
+    }
+    return *it.flip();
+  }
+  template <typename T = Left,
+            std::enable_if_t<std::is_default_constructible_v<T>, int> = 0>
+  left_t const& at_right_or_default(right_t const& key) {
+    auto it = find_right(key);
+    if (it == end_right()) {
+      if (find_left(left_t()) != end_left()) {
+        erase_left(left_t());
+      }
+      insert(left_t(), key);
+      it = find_right(key);
+    }
+    return *it.flip();
+  }
 
   // lower и upper bound'ы по каждой стороне
   // Возвращают итераторы на соответствующие элементы
   // Смотри std::lower_bound, std::upper_bound.
-  left_iterator lower_bound_left(const left_t &left) const;
-  left_iterator upper_bound_left(const left_t &left) const;
+  left_iterator lower_bound_left(const left_t& left) const {
+    return left_iterator(left_set.lower_bound(left));
+  }
+  left_iterator upper_bound_left(const left_t& left) const {
+    return left_iterator(left_set.upper_bound(left));
+  }
 
-  right_iterator lower_bound_right(const right_t &left) const;
-  right_iterator upper_bound_right(const right_t &left) const;
+  right_iterator lower_bound_right(const right_t& right) const {
+    return right_iterator(right_set.lower_bound(right));
+  }
+  right_iterator upper_bound_right(const right_t& right) const {
+    return right_iterator(right_set.upper_bound(right));
+  }
 
   // Возващает итератор на минимальный по порядку left.
-  left_iterator begin_left() const;
+  left_iterator begin_left() const {
+    return left_iterator(left_set.begin_ptr());
+  }
   // Возващает итератор на следующий за последним по порядку left.
-  left_iterator end_left() const;
+  left_iterator end_left() const {
+    return left_iterator(left_set.end_ptr());
+  }
 
   // Возващает итератор на минимальный по порядку right.
-  right_iterator begin_right() const;
+  right_iterator begin_right() const {
+    return right_iterator(right_set.begin_ptr());
+  }
   // Возващает итератор на следующий за последним по порядку right.
-  right_iterator end_right() const;
+  right_iterator end_right() const {
+    return right_iterator(right_set.end_ptr());
+  }
 
   // Проверка на пустоту
-  bool empty() const;
+  bool empty() const {
+    return bimap_size == 0;
+  }
 
   // Возвращает размер бимапы (кол-во пар)
-  std::size_t size() const;
+  std::size_t size() const {
+    return bimap_size;
+  }
 
-  // операторы сравнения
-  friend bool operator==(bimap const &a, bimap const &b);
-  friend bool operator!=(bimap const &a, bimap const &b);
+  friend bool operator==(bimap const& a, bimap const& b) {
+    if (a.bimap_size != b.bimap_size) {
+      return false;
+    }
+
+    for (auto a_it = a.begin_left(), b_it = b.begin_left();
+         b_it != b.end_left(); a_it++, b_it++) {
+      if (*a_it != *b_it) {
+        return false;
+      }
+      if (*(a_it.flip()) != *(b_it.flip())) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  friend bool operator!=(bimap const& a, bimap const& b) {
+    return !(a == b);
+  }
 };
-
---
+++ set.h
@@ -0,0 +1,395 @@
+#pragma once
+
+#include <utility>
+
+namespace intrusive {
+
+struct set_element_base {
+  set_element_base* left{nullptr};
+  set_element_base* right{nullptr};
+  set_element_base* parent{nullptr};
+  std::size_t height = 1;
+};
+
+set_element_base* get_max_node_ptr(set_element_base* pointer) {
+  while (pointer->right) {
+    pointer = pointer->right;
+  }
+  return pointer;
+}
+set_element_base* get_min_node_ptr(set_element_base* pointer) {
+  while (pointer->left) {
+    pointer = pointer->left;
+  }
+  return pointer;
+}
+
+set_element_base* next(set_element_base* pointer) {
+  if (pointer->right) {
+    pointer = pointer->right;
+    return get_min_node_ptr(pointer);
+  }
+  while (pointer->parent && pointer->parent->right == pointer) {
+    pointer = pointer->parent;
+  }
+  return pointer->parent;
+}
+set_element_base* prev(set_element_base* pointer) {
+  if (pointer->left) {
+    return get_max_node_ptr(pointer->left);
+  }
+  while (pointer->parent && pointer->parent->left == pointer) {
+    pointer = pointer->parent;
+  }
+  return pointer->parent;
+}
+
+template <class T, class type_T, class Tag,
+          class Comparator = std::less<type_T>>
+struct set : Comparator { /// AVL-tree
+
+  set_element_base fictitious;
+  set_element_base* root;
+
+  explicit set(Comparator compare = Comparator()) : Comparator(compare) {
+    root = &fictitious;
+    root->parent = root;
+  }
+
+  set(set const& other) = delete;
+
+  Comparator const& cmp() const {
+    return static_cast<const Comparator&>(*this);
+  }
+
+  void upd(set_element_base* pointer) {
+    pointer->height =
+        std::max(height(pointer->left), height(pointer->right)) + 1;
+  }
+
+  void left_rotate(set_element_base* pointer) {
+    set_element_base* ptr = pointer;
+    pointer = pointer->left;
+
+    ptr->left = pointer->right;
+    if (ptr->left) {
+      ptr->left->parent = ptr;
+    }
+
+    pointer->parent = ptr->parent;
+
+    if (pointer->parent->left == ptr) {
+      pointer->parent->left = pointer;
+    } else {
+      pointer->parent->right = pointer;
+    }
+
+    pointer->right = ptr;
+    ptr->parent = pointer;
+
+    upd(ptr);
+    upd(pointer);
+  }
+
+  void right_rotate(set_element_base* pointer) {
+    set_element_base* ptr = pointer;
+    pointer = pointer->right;
+
+    ptr->right = pointer->left;
+    if (ptr->right) {
+      ptr->right->parent = ptr;
+    }
+
+    pointer->parent = ptr->parent;
+    if (pointer->parent->left == ptr) {
+      pointer->parent->left = pointer;
+    } else {
+      pointer->parent->right = pointer;
+    }
+
+    pointer->left = ptr;
+    ptr->parent = pointer;
+
+    upd(ptr);
+    upd(pointer);
+  }
+
+  std::size_t height(set_element_base* ptr) {
+    return (ptr == nullptr ? 0 : ptr->height);
+  }
+
+  std::size_t balance(set_element_base* pointer) {
+    if (height(pointer->left) > height(pointer->right)) {
+      return height(pointer->left) - height(pointer->right);
+    }
+    return height(pointer->right) - height(pointer->left);
+  }
+
+  void correcter(set_element_base* pointer) {
+    upd(pointer);
+    if (balance(pointer) < 2) {
+      return;
+    }
+
+    if (height(pointer->left) > height(pointer->right)) {
+      auto ptr = pointer->left;
+      if (height(ptr->left) > height(ptr->right)) {
+        left_rotate(pointer);
+      } else {
+        right_rotate(ptr);
+        left_rotate(pointer);
+      }
+    } else {
+      auto ptr = pointer->right;
+      if (height(ptr->left) < height(ptr->right)) {
+        right_rotate(pointer);
+      } else {
+        left_rotate(ptr);
+        right_rotate(pointer);
+      }
+    }
+  }
+
+  static type_T& get_value(set_element_base* pointer) {
+    return static_cast<T&>(*pointer).value;
+  }
+
+  set_element_base* lower_bound(const type_T& value,
+                                set_element_base* pointer) const {
+    if (pointer == nullptr) {
+      return root;
+    }
+    if (get_value(pointer) == value) {
+      return pointer;
+    }
+    if (cmp()(get_value(pointer), value)) {
+      if (pointer->right) {
+        return lower_bound(value, pointer->right);
+      }
+      return next(pointer);
+    } else {
+      if (pointer->left) {
+        return lower_bound(value, pointer->left);
+      }
+      return pointer;
+    }
+  }
+
+  set_element_base* lower_bound(const type_T& value) const {
+    return lower_bound(value, root->left);
+  }
+
+  set_element_base* upper_bound(const type_T& value) const {
+    set_element_base* tmp_pointer = lower_bound(value);
+    if (tmp_pointer == root) {
+      return root;
+    }
+    if (get_value(tmp_pointer) == value) {
+      return next(tmp_pointer);
+    }
+    return tmp_pointer;
+  }
+
+  set_element_base* find_ptr(const type_T& value) const {
+    set_element_base* pointer = lower_bound(value);
+
+    if (pointer == root) {
+      return root;
+    }
+    if (get_value(pointer) != value) {
+      return root;
+    }
+
+    return pointer;
+  }
+
+  void insert(T& element, set_element_base* pointer) {
+    auto& tree_value = get_value(pointer);
+    if (cmp()(element.value, tree_value)) {
+      if (pointer->left) {
+        insert(element, pointer->left);
+      } else {
+        pointer->left = &static_cast<set_element_base&>(element);
+        pointer->left->parent = pointer;
+        upd(pointer);
+        return;
+      }
+    } else {
+      if (pointer->right) {
+        insert(element, pointer->right);
+      } else {
+        pointer->right = &static_cast<set_element_base&>(element);
+        pointer->right->parent = pointer;
+        upd(pointer);
+        return;
+      }
+    }
+    correcter(pointer);
+  }
+
+  void insert(T& element) {
+    if (root->left == nullptr) {
+      root->left = &static_cast<set_element_base&>(element);
+      root->left->parent = root;
+      return;
+    }
+    insert(element, root->left);
+  }
+
+  void erase_one_child(set_element_base* pointer) {
+    set_element_base* child_ptr;
+    if (pointer->left) {
+      child_ptr = pointer->left;
+    } else {
+      child_ptr = pointer->right;
+    }
+
+    child_ptr->parent = pointer->parent;
+    if (pointer->parent->left == pointer) {
+      pointer->parent->left = child_ptr;
+    } else {
+      pointer->parent->right = child_ptr;
+    }
+    upd(child_ptr->parent);
+  }
+
+  void swap_link_in_edge(set_element_base* up, set_element_base* down) {
+    auto global_parent = up->parent;
+
+    if (up->left == down) {
+      auto up_right_child = up->right;
+
+      up->left = down->left;
+      up->right = down->right;
+      up->parent = down;
+
+      if (up->left) {
+        up->left->parent = up;
+      }
+      if (up->right) {
+        up->right->parent = up;
+      }
+
+      down->left = up;
+      down->right = up_right_child;
+    } else {
+      auto up_left_child = up->left;
+
+      up->left = down->left;
+      up->right = down->right;
+      up->parent = down;
+
+      if (up->left) {
+        up->left->parent = up;
+      }
+      if (up->right) {
+        up->right->parent = up;
+      }
+
+      down->right = up;
+      down->left = up_left_child;
+    }
+
+    if (down->left) {
+      down->left->parent = down;
+    }
+    if (down->right) {
+      down->right->parent = down;
+    }
+
+    if (global_parent->left == up) {
+      global_parent->left = down;
+    } else {
+      global_parent->right = down;
+    }
+
+    down->parent = global_parent;
+
+    upd(up);
+    upd(down);
+  }
+
+  void swap_link(set_element_base* up, set_element_base* down) {
+    if (up->left == down || up->right == down) {
+      swap_link_in_edge(up, down);
+      return;
+    }
+
+    if (up->left) {
+      up->left->parent = down;
+    }
+    if (up->right) {
+      up->right->parent = down;
+    }
+    if (down->left) {
+      down->left->parent = up;
+    }
+    if (down->right) {
+      down->right->parent = up;
+    }
+
+    std::swap(up->left, down->left);
+    std::swap(up->right, down->right);
+    std::swap(up->parent, down->parent);
+
+    if (up->parent->left == down) {
+      up->parent->left = up;
+    } else {
+      up->parent->right = up;
+    }
+
+    if (down->parent->left == up) {
+      down->parent->left = down;
+    } else {
+      down->parent->right = down;
+    }
+
+    upd(up);
+    upd(down);
+  }
+
+  set_element_base* erase(set_element_base* pointer) {
+    if (!(pointer->left) && !(pointer->right)) {
+      auto parent_ptr = pointer->parent;
+      if (parent_ptr->left == pointer) {
+        parent_ptr->left = nullptr;
+      } else {
+        parent_ptr->right = nullptr;
+      }
+      return parent_ptr;
+    }
+    if (!(pointer->left) || !(pointer->right)) {
+      auto tmp = pointer->parent;
+      erase_one_child(pointer);
+      return tmp;
+    }
+
+    set_element_base* aim_node_ptr = get_max_node_ptr(pointer->left);
+    swap_link(pointer, aim_node_ptr);
+    return erase(pointer);
+  }
+
+  void erase(const type_T& value) {
+    set_element_base* pointer = find_ptr(value);
+    if (pointer == root) {
+      return;
+    }
+    pointer = erase(pointer);
+    while (pointer != root) {
+      auto tmp_ptr = pointer->parent;
+      correcter(pointer);
+      pointer = tmp_ptr;
+    }
+  }
+
+  set_element_base* begin_ptr() const {
+    if (root->left) {
+      return get_min_node_ptr(root->left);
+    }
+    return root;
+  }
+  set_element_base* end_ptr() const {
+    return root;
+  }
+};
+} // namespace intrusive
